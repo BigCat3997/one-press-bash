51a0d970bbf75ffa3938c9732f19ad9feb428e90
bigcat3997 <vqt.develop@gmail.com>
2025-04-19 12:25:19 +0700

    apply global_env_vars_manager

--- src/functions/write_diary.sh
+++ src/functions/write_diary.sh
@@ -27,32 +27,37 @@ HOST_PUBLIC_ENV_VARS="${HOST_PUBLIC_ENV_VARS:-}"
 HOST_PRIVATE_ENV_VARS="${HOST_PRIVATE_ENV_VARS:-}"
 #============================================
 
-if [[ "$IS_IMAGE_TAG_BASED_ON_ENV" == "true" ]]; then
-    IMAGE_TAGS=$(echo "$DOCKER_MULTIPLE_TAGS_ENVS" | jq -c 'reduce .[] as $env ({}; .[$env] = ($env + "." + $ENV.DOCKER_IMAGE_TAG))')
-else
-    IMAGE_TAGS=$(jq -n --arg tag "$DOCKER_IMAGE_TAG" '{"default":$tag}')
-fi
+main() {
+    source ${GLOBAL_ENV_VAR_MANAGER_SCRIPT_PATH}
+    activate_env_vars
 
-PUBLISHER=$(jq -n \
-    --arg git_url "$GIT_URL" \
-    --arg git_commit_id "$GIT_COMMIT_ID" \
-    --arg git_short_commit_id "$GIT_SHORT_COMMIT_ID" \
-    --arg pipeline_name "$PIPELINE_NAME" \
-    --arg build_number "$BUILD_NUMBER" \
-    --arg docker_server_uri "$DOCKER_SERVER_URI" \
-    --argjson image_tags "$IMAGE_TAGS" \
-    '{
-        git_url: $git_url,
-        git_commit_id: $git_commit_id,
-        git_short_commit_id: $git_short_commit_id,
-        pipeline_name: $pipeline_name,
-        build_number: $build_number,
-        docker_server_uri: $docker_server_uri,
-        image_tags: $image_tags,
-    }')
+    if [[ "$IS_IMAGE_TAG_BASED_ON_ENV" == "true" ]]; then
+        IMAGE_TAGS=$(echo "$DOCKER_MULTIPLE_TAGS_ENVS" | jq -c 'reduce .[] as $env ({}; .[$env] = ($env + "." + $ENV.DOCKER_IMAGE_TAG))')
+    else
+        IMAGE_TAGS=$(jq -n --arg tag "$DOCKER_IMAGE_TAG" '{"default":$tag}')
+    fi
 
-PUBLISH_FILE_PATH="${PUBLISH_PREFIX_PATH}/${PUBLISH_FILE_NAME}"
-echo "$PUBLISHER" | jq '.' > "$PUBLISH_FILE_PATH"
+    PUBLISHER=$(jq -n \
+        --arg git_url "$GIT_URL" \
+        --arg git_commit_id "$GIT_COMMIT_ID" \
+        --arg git_short_commit_id "$GIT_SHORT_COMMIT_ID" \
+        --arg pipeline_name "$PIPELINE_NAME" \
+        --arg build_number "$BUILD_NUMBER" \
+        --arg docker_server_uri "$DOCKER_SERVER_URI" \
+        --argjson image_tags "$IMAGE_TAGS" \
+        '{
+            git_url: $git_url,
+            git_commit_id: $git_commit_id,
+            git_short_commit_id: $git_short_commit_id,
+            pipeline_name: $pipeline_name,
+            build_number: $build_number,
+            docker_server_uri: $docker_server_uri,
+            image_tags: $image_tags,
+        }')
 
-source ${GLOBAL_ENV_VAR_MANAGER_SCRIPT_PATH} 
-write_env_vars "export PUBLISH_FILE_PATH=${PUBLISH_FILE_PATH}"
+    PUBLISH_FILE_PATH="${PUBLISH_PREFIX_PATH}/${PUBLISH_FILE_NAME}"
+    echo "$PUBLISHER" | jq '.' > "$PUBLISH_FILE_PATH"
+    write_env_vars "export PUBLISH_FILE_PATH=${PUBLISH_FILE_PATH}"
+}
+
+main "$@"
