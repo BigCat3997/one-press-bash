369a33084f61c9b3ee39404a96c61d9febc940c4
bigcat3997 <vqt.develop@gmail.com>
2025-06-08 18:06:57 +0700

    use backend-config for this step

--- src/terraform-apply.sh
+++ src/terraform-apply.sh
@@ -9,6 +9,10 @@ TARGET_TERRAFORM_MODULE_PATH
 EOF
 
 cd $TARGET_TERRAFORM_MODULE_PATH
-terraform init
+terraform init \
+    -backend-config="resource_group_name=${AZURE_BACKEND_RESOURCE_GROUP}" \
+    -backend-config="storage_account_name=${AZURE_STORAGE_ACCOUNT_NAME}" \
+    -backend-config="container_name=${AZURE_CONTAINER_NAME}" \
+    -backend-config="key=${AZURE_BLOB_KEY}"
 terraform apply --auto-approve
 cd - > /dev/null
