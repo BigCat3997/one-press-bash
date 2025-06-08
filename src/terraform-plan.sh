fc97d151391663a4a3f18cf4b112c8fb5060f16e
bigcat3997 <vqt.develop@gmail.com>
2025-06-08 16:23:46 +0700

    init project

---
+++ src/terraform-plan.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Purpose: Run Terraform plan.
+# --------------------------------------
+
+while read var; do
+    [ -z "${!var}" ] && { echo "$var is empty or not set."; exit 1; }
+done << EOF
+TARGET_TERRAFORM_MODULE_PATH
+AZURE_BACKEND_RESOURCE_GROUP
+AZURE_STORAGE_ACCOUNT_NAME
+AZURE_CONTAINER_NAME
+AZURE_BLOB_KEY
+ARM_SUBSCRIPTION_ID
+ARM_TENANT_ID
+ARM_CLIENT_ID
+ARM_CLIENT_SECRET
+EOF
+
+set -e
+if [ -z "$LOCAL_DATA_PATH" ]; then
+    echo "Do not use local data file."
+else
+    echo "Use local data file."
+    cp -r $LOCAL_DATA_PATH/. $TARGET_TERRAFORM_MODULE_PATH
+
+    echo "Verify files."
+    tree $TARGET_TERRAFORM_MODULE_PATH
+fi
+
+cd $TARGET_TERRAFORM_MODULE_PATH
+terraform init \
+    -backend-config="resource_group_name=${AZURE_BACKEND_RESOURCE_GROUP}" \
+    -backend-config="storage_account_name=${AZURE_STORAGE_ACCOUNT_NAME}" \
+    -backend-config="container_name=${AZURE_CONTAINER_NAME}" \
+    -backend-config="key=${AZURE_BLOB_KEY}"
+terraform plan
+
+cd - > /dev/null
