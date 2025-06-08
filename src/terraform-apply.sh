3eb4a1f867ca848b968746a00d3a9908e0b59fa1
bigcat3997 <vqt.develop@gmail.com>
2025-06-08 18:01:06 +0700

    remove cp files in this step

--- src/terraform-apply.sh
+++ src/terraform-apply.sh
@@ -8,17 +8,6 @@ done << EOF
 TARGET_TERRAFORM_MODULE_PATH
 EOF
 
-set -e
-if [ -z "$LOCAL_DATA_PATH" ]; then
-    echo "Do not use local data file."
-else
-    echo "Use local data file."
-    cp -r $LOCAL_DATA_PATH/. $TARGET_TERRAFORM_MODULE_PATH
-
-    echo "Verify files."
-    tree $TARGET_TERRAFORM_MODULE_PATH
-fi
-
 cd $TARGET_TERRAFORM_MODULE_PATH
 terraform init
 terraform apply --auto-approve
--- src/terraform-destroy.sh
+++ src/terraform-destroy.sh
@@ -3,21 +3,10 @@
 # --------------------------------------
 
 while read var; do
-    [ -z "${!var}" ] && { echo "$var is empty or not set."; exit 1; }
+    [ -z "${!var}" ] && { echo "$var is empty or not set."; exit ; }
 done << EOF
 TARGET_TERRAFORM_MODULE_PATH
 EOF
-
-set -e
-if [ -z "$LOCAL_DATA_PATH" ]; then
-    echo "Do not use local data file."
-else
-    echo "Use local data file."
-    cp -r $LOCAL_DATA_PATH/. $TARGET_TERRAFORM_MODULE_PATH
-
-    echo "Verify files."
-    tree $TARGET_TERRAFORM_MODULE_PATH
-fi
 
 cd $TARGET_TERRAFORM_MODULE_PATH
 terraform init
